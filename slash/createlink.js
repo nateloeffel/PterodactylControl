c4791bfaf35b088019bf25249a6acd20ef89304b
nateloeffel <nateloeffel@inqz.net>
2023-09-24 00:23:54 -0400

    added ability to create hypixel beaming links

--- slash/createlink.js
+++ slash/createlink.js
@@ -10,6 +10,9 @@ const {
 	SlashCommandBuilder,
 	SelectMenuBuilder,
 } = require("discord.js");
+require("dotenv").config()
+
+const PTERODACTYL_API_KEY = process.env.PTERODACTYL_API_KEY
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -41,13 +44,67 @@ module.exports = {
 		),
 
 	run: async ({ client, interaction }) => {
+		if (interaction.user.id !== "1127635089761763358") return await interaction.reply("You may not use this command.")
 
-        // validate the request
+		const linkname = interaction.options.getString("linkname")
 
-        // set message status to thinking while api request is being sent
+		// validate the request
+		if (interaction.options.getString("type") == "lunar") return await interaction.reply("This feature is not enabled yet.")
+		// set message status to thinking while api request is being sent
+		await interaction.deferReply()
 
-        // give them the information of their new server
 
-		return await interaction.reply("Pong!");
+		const data = {
+			"name": linkname,
+			"user": 1,
+			"egg": 28,
+			"docker_image": "ghcr.io/parkervcp/yolks:nodejs_20",
+			"startup": "if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == \"1\" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; if [[ \"${MAIN_FILE}\" == \"*.js\" ]]; then /usr/local/bin/node \"/home/container/${MAIN_FILE}\" ${NODE_ARGS}; else /usr/local/bin/ts-node \"/home/container/${MAIN_FILE}\" ${NODE_ARGS}; fi",
+			"environment": {
+				"GIT_ADDRESS": "https://github.com/nateloeffel/hypixel-link",
+				"BRANCH": "main",
+				"USER_UPLOAD": "0",
+				"AUTO_UPDATE": "1",
+				"USERNAME": "nateloeffel",
+				"ACCESS_TOKEN": "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",
+				"MAIN_FILE": "index.js"
+			},
+			"limits": {
+				"memory": 230,
+				"swap": 0,
+				"disk": 512,
+				"io": 500,
+				"cpu": 100
+			},
+			"feature_limits": {
+				"databases": 0,
+				"backups": 1
+			},
+			deploy: {
+				locations: [1],
+				dedicated_ip: false,
+				port_range: [],
+			},
+		}
+
+		const request = await fetch("https://panel.inqz.net/api/application/servers", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": `Bearer ${PTERODACTYL_API_KEY}`
+			},
+			body: JSON.stringify(data)
+		})
+		const response = await request.json()
+		const { id, uuid, name } = response.attributes
+		await interaction.editReply("Server has been created! :white_check_mark:");
+
+		const embed = new EmbedBuilder()
+			.setTitle("New Beaming Link Created!")
+			.setColor("22bb33")
+			.setDescription(`**Name:** \`${name}\`\n**UUID:** \`${uuid}\`\n**ID:** \`${id}\`\n`)
+		// give them the information of their new server
+		await interaction.followUp({ embeds: [embed] })
 	},
 };
